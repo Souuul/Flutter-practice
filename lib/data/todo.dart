fb4cc9e7e796ed7fad1241ccc654a8ab6c429909
Sol Han <hans53400@naver.com>
2021-11-07 14:17:19 +0900

    todo list first commit

---
+++ lib/data/todo.dart
@@ -0,0 +1,9 @@
+class Todo {
+  String title;
+  String memo;
+  String category;
+  int color;
+  int done;
+  int date;
+  Todo({required this.title, required this.memo, required this.category, required this.color, required this.done, required this.date});
+}
---
+++ lib/data/util.dart
@@ -0,0 +1,18 @@
+class utils {
+  static int getFormatTime(DateTime date){
+    return int.parse('${date.year}${makeTwoDigit(date.month)}${makeTwoDigit(date.day)}');
+  }
+  
+  static DateTime numToDateTime(int date){
+    String _d = date.toString();
+    int year = int.parse(_d.substring(0,4));
+    int month = int.parse(_d.substring(4,6));
+    int day = int.parse(_d.substring(6,8));
+    
+    return DateTime(year, month, day);
+  }
+  
+  static String makeTwoDigit(int num){
+    return num.toString().padLeft(2, '0');
+  }
+}
